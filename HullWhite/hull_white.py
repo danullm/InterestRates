0842cabbc57c8e9129b7afbe88f2a957f6a18912
Unknown <daniel-ullmann@gmx.de>
2018-04-16 17:02:48 +0200

    fixed

--- HullWhite/hull_white.py
+++ HullWhite/hull_white.py
@@ -21,7 +21,7 @@ from vasicek import *
 
 def hull_white_forward(r0, theta, kappa, sigma, beta0, beta1, beta2, beta3, tau1, tau2, T = 10, N = 50):
     t = np.linspace(0,T,N)
-    phi = phi_svensson(r0, theta, kappa, sigma, beta0, beta1, beta2, beta2, tau1, tau2, t)
+    phi = phi_svensson(r0, theta, kappa, sigma, beta0, beta1, beta2, beta3, tau1, tau2, t)
     vasicek = vasicek_A_prime(kappa, theta, sigma, t) + vasicek_B_prime(kappa, t)*r0
     f0 = phi + vasicek
     f0 = pd.DataFrame(data = f0, index = t)
@@ -30,7 +30,7 @@ def hull_white_forward(r0, theta, kappa, sigma, beta0, beta1, beta2, beta3, tau1
 def phi_svensson(r0, theta, kappa, sigma, beta0, beta1, beta2, beta3, tau1, tau2, t):
     f0 = svensson_forwards(beta0, beta1, beta2, beta3, tau1, tau2, t)
     vasicek = vasicek_A_prime(kappa, theta, sigma, t) + vasicek_B_prime(kappa, t)*r0
-    phi2 = f02 - vasicek
+    phi2 = f0 - vasicek
     return(phi2)
     
 
@@ -41,23 +41,20 @@ if __name__ == '__main__':
     [beta0, beta1, beta2, beta3, tau1, tau2] = \
         np.array([ 1.60913/100, -2.25762/100, -2.88699/100,  0.02519/100,  2.17563,  0.18767])
     
-    [r0, theta, kappa, sigma] = np.array([-0.01235011,  0.01870044,  0.2178629 ,  0.01442659])
+    [r0, theta, kappa, sigma] = np.array([-0.01020897,  0.17611249,  0.12063917,  0.18981454])
     
-    t = np.arange(1./12,10,1./12)
-
+    T = 10
+    
+    t = np.arange(0,T,1)
+    
     f0 = svensson_forwards(beta0, beta1, beta2, beta3, tau1, tau2, t)
+    vasicek_fit = vasicek_forward_curve(r0, theta, kappa, sigma, T)
+    hw_fit = hull_white_forward(r0, theta, kappa, sigma, beta0, beta1, beta2, beta3, tau1, tau2, T)
+
     
-    vasicek_fit = vasicek_forward_curve(r0, theta, kappa, sigma, T = max(t))
-    vasicek = vasicek_A_prime(kappa, theta, sigma, t) + vasicek_B_prime(kappa, t)*r0
-    
-    phi = phi_svensson(r0, theta, kappa, sigma, beta0, beta1, beta2, beta3, tau1, tau2, t)
-    
-    
-    plt.scatter(t,f0, label = 'Market') 
-    
-    plt.plot(t,vasicek, label = 'Vasicek')
-    plt.plot(vasicek_fit, label = 'Vasicek2')
-    #plt.plot(hw_fit, label = 'Hull White')
+    plt.scatter(t,f0, label = 'Market', marker = '+', c = 'red') 
+    plt.plot(vasicek_fit, label = 'Vasicek')
+    plt.plot(hw_fit, label = 'Hull White')
     plt.legend()
     plt.grid()
     plt.tight_layout()
